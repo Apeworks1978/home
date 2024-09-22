9817f66e809ba6beb8ef5e7e52a0fcdfbf37df81
Masaru WATANABE <watanabe.masaru@hotmail.com>
2024-09-22 10:44:23 +0900

    add app folder.

--- __init.bat
+++
@@ -1,7 +0,0 @@
-echo "# home" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Apeworks1978/home.git
-git push -u origin main
